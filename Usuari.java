0dad9917f941e325b4312b14870911156ea52a2d
XPandele <xavieragustinpandele@inspladelestany.cat>
2022-10-11 19:06:09 +0200

    Comentari en la Branca Xavi

--- Usuari.java
+++ Usuari.java
@@ -1,6 +1,6 @@
-import java.time.LocalDate;
+import java.time.LocalDate;//Comentari en la BrancaXavi
 
-public class Usuari { //Comentari en la BrancaXavi
+public class Usuari {
     private final int id;
     private static int ultimId = 0;
 
